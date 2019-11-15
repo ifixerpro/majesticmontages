8192d958e37f773698b32991ab6250dc1483f872
Jake Brown <33910557+ifixerpro@users.noreply.github.com>
2019-11-15 10:33:49 -0500

    Update form.php

--- form.php
+++ form.php
@@ -1,1 +1,12 @@
+<?php
 
+$fname = $_GET['fname'];
+$lname = $_GET['lname'];
+$email = $_GET['email'];
+$subject = $_GET['subject'];
+$message = $_GET['message'];
+
+echo "Welcome Mr. ".$lname;
+echo "<br/>";
+
+?>
